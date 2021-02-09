daed41c3dc74a2c83af9643be589a4fe446d2574
wangyimin <wangyimin@yahoo.com>
2021-02-09 15:53:34 +0900

    updated

--- TaskApp1/PC.cs
+++ TaskApp1/PC.cs
@@ -9,30 +9,20 @@ namespace TaskApp1
     {
         private ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
 
-        public int Length { get => _queue.Count; }
+        public int Length => _queue.Count;
 
         public Task Produce(T element, Action<T> onCompleted)
         {
-            Task t = Task.Factory.StartNew(
-                () =>
-                {
-                    Add(element, onCompleted);
-                }
+            return Task.Factory.StartNew(
+                () => Add(element, onCompleted)
                 );
-
-            return t;
         }
 
         public Task Consume(Action<T> onCompleted)
         {
-            Task t = Task.Factory.StartNew(
-                () =>
-                {
-                    Remove(onCompleted);
-                }
+            return Task.Factory.StartNew(
+                () => Remove(onCompleted)
                 );
-
-            return t;
         }
 
         private void Add(T element, Action<T> onCompleted)
@@ -46,14 +36,11 @@ namespace TaskApp1
             _queue.TryDequeue(out T el);
 
             if (el == null)
-                Trace.WriteLine($"Consume is waiting...");
+                Trace.WriteLine($"Consume is idle...");
             else
                 if (onCompleted != null) onCompleted(el);
         }
 
-        public void Dispose()
-        {
-            _queue.Clear();
-        }
+        public void Dispose() => _queue.Clear();
     }
 }
