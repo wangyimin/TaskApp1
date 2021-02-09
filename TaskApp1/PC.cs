e84da6268e7bdc2a1c2ad4d0d37cf5c40460773c
Wang Yimin <wangyimin@yahoo.com>
2021-02-09 11:37:25 +0900

    プロジェクト ファイルを追加します。

---
+++ TaskApp1/PC.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TaskApp1
+{
+    class PC<T> : IDisposable
+    {
+        private ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+
+        public int Length { get => _queue.Count; }
+
+        public Task Produce(T element, Action<T> onCompleted)
+        {
+            Task t = Task.Factory.StartNew(
+                () =>
+                {
+                    Add(element, onCompleted);
+                }
+                );
+
+            return t;
+        }
+
+        public Task Consume(Action<T> onCompleted)
+        {
+            Task t = Task.Factory.StartNew(
+                () =>
+                {
+                    Remove(onCompleted);
+                }
+                );
+
+            return t;
+        }
+
+        private void Add(T element, Action<T> onCompleted)
+        {
+            _queue.Enqueue(element);
+            if (onCompleted != null) onCompleted(element);
+        }
+
+        private void Remove(Action<T> onCompleted)
+        {
+            _queue.TryDequeue(out T el);
+
+            if (el == null)
+                Trace.WriteLine($"Consume is waiting...");
+            else
+                if (onCompleted != null) onCompleted(el);
+        }
+
+        public void Dispose()
+        {
+            _queue.Clear();
+        }
+    }
+}
---
+++ TaskApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            DateTime endP = start.AddSeconds(5);
+            DateTime endC = start.AddSeconds(8);
+
+            using (PC<string> _pc = new PC<string>())
+            {
+                Task _p = Task.Factory.StartNew(
+                    () =>
+                    {
+                        while (DateTime.Now < endP)
+                        {
+                            Task t = _pc.Produce(GetDateTime(), el => { Trace.WriteLine($"Produce ({el}) is coming"); });
+                            Thread.Sleep(new Random().Next(1000));
+                        }
+                    });
+
+                Task _c = Task.Factory.StartNew(
+                    () =>
+                    {
+                        while (DateTime.Now < endC || _pc.Length > 0)
+                        {
+                            Thread.Sleep(800);
+                            Task t = _pc.Consume(el => { Trace.WriteLine($"Consume ({el}) is completed at {GetDateTime()}"); });
+                        }
+                    });
+
+                Task.WaitAll(_p, _c);
+            }
+        }
+
+        static string GetDateTime() => DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
+    }
+}
